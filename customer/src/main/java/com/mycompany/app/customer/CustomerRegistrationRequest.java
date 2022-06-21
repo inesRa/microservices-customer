9978b482deca9ed003d46e8e7daaec89cf73603d
RATTAB INASS <IRATTAB@royalairmaroc.com>
2022-06-21 16:26:10 +0100

    first commit

---
+++ customer/src/main/java/com/mycompany/app/customer/CustomerRegistrationRequest.java
@@ -0,0 +1,4 @@
+package com.mycompany.app.customer;
+
+public record CustomerRegistrationRequest(String firstName, String lastName, String email) {
+}
---
+++ customer/src/main/java/com/mycompany/app/customer/CustomerRepository.java
@@ -0,0 +1,6 @@
+package com.mycompany.app.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+}
